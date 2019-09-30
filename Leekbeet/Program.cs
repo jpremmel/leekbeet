a37ce1dbe4e7bbd54d4cc9638980aa0bd4434e4d
Jacqueline Remmel and Mike McShane <jacqueline@>
2019-09-30 13:59:32 -0700

    add IndexOf if statement to exclude inputted words.

--- Leekbeet/Program.cs
+++ Leekbeet/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to LeekBeet! Enter a phrase to translate:");
+        Console.WriteLine("Welcome to LeekBeet! Enter a sentence or phrase to translate:");
         string input = Console.ReadLine();
         Leekbeet newLeekbeet = new Leekbeet();
         Console.WriteLine(newLeekbeet.Translate(input));
